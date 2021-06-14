a53326f605996f4d351785f82c12e136d628fd38
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-06-14 17:18:04 -0300

    register-product

--- back-end/controladores/produtos.js
+++ back-end/controladores/produtos.js
@@ -33,6 +33,46 @@ const obterProduto = async (req, res) => {
 }
 
 const cadastrarProduto = async (req, res) => {
+
+//   nome
+// estoque
+// preco
+// descricao 
+// Cadastrar o produto no banco de dados para o id do usuario logado  
+  
+  
+  try {
+    
+    const { nome, estoque, preco, descricao, imagem } = req.body
+    
+    if (!nome) {
+      return res.status(400).json("O campo nome é obrigatorio.")
+    }
+
+      if (!estoque) {
+      return res.status(400).json("O campo estoque é obrigatorio.")
+    }
+
+      if (!preco) {
+      return res.status(400).json("O campo preco é obrigatorio.")
+    }
+
+      if (!descricao) {
+      return res.status(400).json("O campo descricao é obrigatorio.")
+    }
+    
+    const produto = await conexao.query('insert into produtos (nome, estoque, preco, descricao, imagem) values($1, $2, $3, $4, $5)', [nome, estoque, preco, descricao, imagem])
+      
+      if (produto.rowCount === 0) {
+      return res.status(404).json('Não foi possivel cadastrar o produto')
+    }
+
+    return res.status(200).json("Produto cadastrado com sucesso")
+    
+  } catch (error) {
+    
+    return res.status(400).json(error.message)
+  }
   
 }
 
