95bccddeec24e0ea63db9069a82a8d86e95244a1
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-06-14 20:29:09 -0300

    updating-product

--- back-end/controladores/produtos.js
+++ back-end/controladores/produtos.js
@@ -34,12 +34,7 @@ const obterProduto = async (req, res) => {
 
 const cadastrarProduto = async (req, res) => {
 
-//   nome
-// estoque
-// preco
-// descricao 
 // Cadastrar o produto no banco de dados para o id do usuario logado  
-  
   
   try {
     
@@ -63,8 +58,8 @@ const cadastrarProduto = async (req, res) => {
     
     const produto = await conexao.query('insert into produtos (nome, estoque, preco, descricao, imagem) values($1, $2, $3, $4, $5)', [nome, estoque, preco, descricao, imagem])
       
-      if (produto.rowCount === 0) {
-      return res.status(404).json('Não foi possivel cadastrar o produto')
+    if (produto.rowCount === 0) {
+    return res.status(404).json('Não foi possivel cadastrar o produto')
     }
 
     return res.status(200).json("Produto cadastrado com sucesso")
@@ -78,9 +73,61 @@ const cadastrarProduto = async (req, res) => {
 
 const atualizarProduto = async (req, res) => {
   
+// Buscar o produto no banco de dados pelo id informado na rota
+// Validar se o produto existe e se pertence ao usuário logado
+// Validar os campos obrigatórios:
+// nome
+// estoque
+// preco
+// descricao
+// Atualizar o produto no banco de dados
+  
+  const { id } = req.params
+  const { nome, estoque, preco, descricao, imagem } = req.body
+  
+  try {
+    
+    const produto = await conexao.query('select * from produtos where id = $1', [id])
+    
+    if (produto.rowCount === 0) {
+      return res.status(404).json('Produto não encontrado')
+    }
+    
+    if (!nome) {
+      return res.status(400).json("O campo nome é obrigatorio.")
+    }
+
+      if (!estoque) {
+      return res.status(400).json("O campo estoque é obrigatorio.")
+    }
+
+      if (!preco) {
+      return res.status(400).json("O campo preco é obrigatorio.")
+    }
+
+      if (!descricao) {
+      return res.status(400).json("O campo descricao é obrigatorio.")
+    }
+
+    const atualizandoProduto = await conexao.query('update produtos set nome = $1, estoque = $2, preco = $3, descricao = $4, imagem = $5 where id = $6', [nome, estoque, preco, descricao, imagem, id])
+
+    if (atualizandoProduto.rowCount === 0) {
+
+      return res.status(404).json('Não foi possivel atualizar o produto')
+    }
+
+    return res.status(200).json('Produto atualizado com sucesso.')
+    
+  } catch (error) {
+    
+    return res.status(400).json(error.message)
+  }
 }
 
 const excluirProduto = async (req, res) => {
+  
+// Validar se o produto existe e se pertence ao usuário logado
+// Deletar o produto do banco de dados
   
 }
 
