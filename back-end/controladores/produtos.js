dda41b35da085ade9e5af8ce1dbbdecc9b7f5c18
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-06-14 16:44:48 -0300

    getting-one-product

--- back-end/controladores/produtos.js
+++ back-end/controladores/produtos.js
@@ -6,7 +6,7 @@ const listarProdutos = async (req, res) => {
     
     const { rows: produtos } = await conexao.query('select * from produtos')
     return res.status(200).json(produtos)
-    
+
   } catch (error) {
     
     return res.status(400).json(error.message)
@@ -15,6 +15,21 @@ const listarProdutos = async (req, res) => {
 
 const obterProduto = async (req, res) => {
   
+  const {id} = req.params
+  try {
+    
+    const produto = await conexao.query('select * from produtos where id = $1', [id])
+    
+    if (produto.rowCount === 0) {
+      return res.status(404).json('Produto não encontrado')
+    }
+
+    return res.status(200).json(produto.rows[0])
+    
+  } catch (error) {
+    
+    return res.status(400).json(error.message)
+  }
 }
 
 const cadastrarProduto = async (req, res) => {
