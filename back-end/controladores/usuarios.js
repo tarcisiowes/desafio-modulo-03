161c1c9b7eeddbc12305d2a65ce91ea735a0cba8
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-06-15 12:49:09 -0300

    login-user

--- back-end/controladores/usuarios.js
+++ back-end/controladores/usuarios.js
@@ -55,10 +55,56 @@ const cadastrarUsuario = async (req, res) => {
 
 const logarUsuario = async (req, res) => {
 
-// Verificar se o e-mail existe
-// Validar e-mail e senha
 // Criar token de autenticação com id do usuário
 // Retornar um objeto com os dados do usuario (sem a senha) e o token criado
+  
+  const { email, senha } = req.body
+  
+  if (!email) {
+    return res.status(400).json("O campo email é obrigatorio.")
+  }
+
+  if (!senha) {
+    return res.status(400).json("O campo senha é obrigatorio.")
+  }
+
+  try {
+    const usuarios = await conexao.query('select * from usuarios where email = $1', [email])
+    
+    if (usuarios.rowCount === 0) {
+    return res.status(404).json('Email ou senha estão incorretos')
+    }
+
+    const usuario = usuarios.rows[0]
+
+    const result = await pwd.verify(Buffer.from(senha), Buffer.from(usuario.senha, "hex"))
+
+    switch (result) {
+         
+      case securePassword.INVALID_UNRECOGNIZED_HASH:
+      case securePassword.INVALID:
+        return res.status(400).json('Email ou senha estão incorretos')
+      case securePassword.VALID:
+        break
+      case securePassword.VALID_NEEDS_REHASH:
+        try {
+
+          const hash = (await pwd.hash(Buffer.from(senha))).toString("hex")    
+    
+          const usuario = await conexao.query('update usuarios set senha = $1 where email = $2', [hash, email])
+
+        } catch {          
+        }
+        break
+    }
+
+    return res.json(`Bem vindo ${ usuario.nome }`)
+    // email: ${usuario.email }
+    // loja: ${usuario.nome_loja}
+
+  } catch (error) {
+    return res.status(400).json(error.message)
+  }
 }
 
 const perfilUsuario = async (req, res) => {
