c9358e494ccef27fa2f41a56727adeaaf2934c33
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-06-16 15:19:14 -0300

    error-State-login

--- front-end/src/paginas/login/index.js
+++ front-end/src/paginas/login/index.js
@@ -1,9 +1,10 @@
 import './style.js'
 
-import React from 'react'
+import React, { useState } from 'react'
 import useStyles from './style.js';
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import Alert from '@material-ui/lab/Alert'
 import Typography from '@material-ui/core/Typography'
 import { useForm } from 'react-hook-form'
 import { useHistory } from 'react-router-dom';
@@ -13,17 +14,25 @@ export default function Login() {
   const classes = useStyles()
   const { register, handleSubmit } = useForm()
   const history = useHistory()
+  const [erro, setErro] = useState('')
 
   function onSubmit(data) {
 
-    if (data.senha === '123') {
+    setErro('')
+    try {
+      
       history.push('/lista')
+    } catch (error) {
+      
+      setErro(error.message)
     }
   }
   
   return (
 
-    <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+    <form className={ classes.root } noValidate autoComplete="off" onSubmit={ handleSubmit(onSubmit) }>
+      
+      { erro && <Alert severity="error">{ erro }</Alert>}
       <Typography variant="h3">Login</Typography>
       <TextField label="Email" {... register('email')} />
       <TextField label="Senha" {...register('senha')} type="password" />
