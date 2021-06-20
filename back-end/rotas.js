b74e394683e168a6c48f2f26388b95f75e849771
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-06-20 20:52:39 -0300

    position-authy

--- back-end/rotas.js
+++ back-end/rotas.js
@@ -8,16 +8,15 @@ const rotas = express()
 rotas.post('/cadastro', usuarios.cadastrarUsuario)
 rotas.post('/login', usuarios.logarUsuario)
 
-rotas.get('/produtos', produtos.listarProdutos)
-
 rotas.use(authy)
+
 rotas.get('/perfil', usuarios.perfilUsuario)
 rotas.put('/perfil', usuarios.editarUsuario)
 
+rotas.get('/produtos', produtos.listarProdutos)
 rotas.get('/produtos/:id', produtos.obterProduto)
 rotas.post('/produtos', produtos.cadastrarProduto )
 rotas.put('/produtos/:id', produtos.atualizarProduto)
 rotas.delete('/produtos/:id', produtos.excluirProduto)
-
 
 module.exports = rotas
